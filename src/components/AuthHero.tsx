8f68138725414177e68dc884ddbfc68a7731c8cc
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-07-16 00:25:40 +0530

    add image in AuthHero and add headings and input field in Login page

--- src/components/AuthHero.tsx
+++ src/components/AuthHero.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import heroIcon from "../images/hero-icon.webp"
 interface Props {}
 const AuthHero: React.FC<Props> = (props) => {
-  return <div className = "bg-black text-white w-1/2 h-screen">
-      Logo will go here.
+  return <div className = "bg-black text-white w-1/2 h-screen flex items-center justify-center">
+      <img src = {heroIcon} className = "h-3/4 w-3/4 "/>
   </div>;
 };
 AuthHero.defaultProps = {};
--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -2,11 +2,55 @@ import React from "react";
 import { Link } from "react-router-dom";
 interface Props {}
 const Login: React.FC<Props> = (props) => {
-  return <div>
-      This is login page. 
-      <Link to = "/signup">Signup</Link>
-      <Link to = "/dashboard">Go to dashboard</Link>
-  </div>;
+  return (
+    <div className="flex flex-col items-start px-11 py-3 max-w-96 mx-auto">
+      <h1 className="text-4xl mb-2">
+        Log In to <span className="text-blue-600">DevsLane</span>
+      </h1>
+      <h3 className="font-semibold mb-12 ">
+        New here?{" "}
+        <span className="underline text-blue-600">Create an account</span>
+      </h3>
+      <form className = "w-full">
+        <div className="flex  border-b  mt-3 mb-6">
+          <svg
+            className="text-blue-500 "
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+          </svg>
+          <input className="pl-3 pb-3 w-full" placeholder="email"></input>
+        </div>
+        <div className="flex  border-b mt-3 mb-8">
+          <svg
+          className = "text-blue-500"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+            <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+          </svg>
+          <input className="pl-3 pb-3 w-full" placeholder="Password"></input>
+        </div>
+      </form>
+    </div>
+  );
 };
 Login.defaultProps = {};
 
