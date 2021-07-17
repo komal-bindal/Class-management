368293f682705c355eacad9ef9bab6cfdcd03d2b
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-07-17 19:18:24 +0530

    create button.tsx file

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+interface Props {}
+const Button: React.FC<Props> = (props) => {
+  return <div>
+      
+  </div>;
+};
+Button.defaultProps = {};
+
+export default React.memo(Button);
