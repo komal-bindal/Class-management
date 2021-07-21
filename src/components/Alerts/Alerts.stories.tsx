7b284e3e6b34aa41999626ab653c18a47b38899e
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-07-22 00:12:15 +0530

    add alert story

---
+++ src/components/Alerts/Alerts.stories.tsx
@@ -0,0 +1,22 @@
+import Alerts from "./Alerts";
+
+export default{
+    title: "Alerts",
+    component: Alerts,
+    argTypes:{
+        textTheme:{
+            control:{
+                type: "select"
+            }
+        }
+    }
+}
+export const main = (args:any) => <Alerts {...args}></Alerts>
+
+main.args={
+    theme: "solid",
+    textTheme : "primary",
+    title: "Primary!",
+  children: "Lorem Ipsum is simply dummy text of the printing",
+  
+}
