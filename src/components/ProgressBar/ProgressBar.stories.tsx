954fcd503747f8386facd4b1126580a13fab4721
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-07-22 13:01:19 +0530

    create progress bar component ant its story

---
+++ src/components/ProgressBar/ProgressBar.stories.tsx
@@ -0,0 +1,21 @@
+import { prototype } from "events";
+import ProgressBar from "./ProgressBar";
+
+export default {
+  title: "ProgressBar",
+  component: ProgressBar,
+  argTypes: {
+    progress: {
+      control: {
+        type: "range",
+      },
+    },
+  },
+};
+
+export const main = (args: any) => <ProgressBar {...args}></ProgressBar>;
+
+main.args = {
+    progress:50,
+    theme:"primary"
+}
---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface Props {
+  theme?: "primary" | "warning" | "success" | "info" | "error";
+  progress?: number;
+}
+
+const ProgressBar: React.FC<Props> = ({ progress, theme }) => {
+  let themeClasses = "";
+  if (theme == "primary") {
+    themeClasses = "bg-indigo-600";
+  } else if (theme == "warning") {
+    themeClasses = "bg-yellow-600";
+  } else if (theme == "success") {
+    themeClasses = "bg-green-600";
+  } else if (theme == "info") {
+    themeClasses = "bg-blue-600";
+  } else {
+    themeClasses = "bg-red-600";
+  }
+  return (
+    <div className="max-w-3xl w-full relative bg-gray-200 h-3 rounded-lg ">
+      <div
+        className={"h-full absolute rounded-lg " + themeClasses}
+        style={{ width: progress + "%" }}
+      ></div>
+    </div>
+  );
+};
+ProgressBar.defaultProps = {
+  progress: 0,
+  theme: "primary",
+};
+
+export default React.memo(ProgressBar);
