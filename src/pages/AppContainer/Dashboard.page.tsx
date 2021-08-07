b180c205083e94c8f9b6804aa3dee9d1890447c1
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-08-07 13:40:02 +0530

    fix background of dashboard and recordings page

--- src/pages/AppContainer/Dashboard.page.tsx
+++ src/pages/AppContainer/Dashboard.page.tsx
@@ -8,7 +8,7 @@ const Dashboard: React.FC<Props> = () => {
   const user = useAppSelector((state) => authUserSelector(state));
 
   return (
-    <div className="p-10 bg-gray-200">
+    <div className="p-10 bg-gray-100 w-full">
       <h1 className=" text-2xl ">Hello... {user!.first_name}</h1>
       <h3>This is Dashboard</h3>
     </div>
--- src/pages/AppContainer/Recordings.page.tsx
+++ src/pages/AppContainer/Recordings.page.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 interface Props {}
 const Recordings: React.FC<Props> = (props) => {
-  return <div><Link to = "/dashboard">Dashboard</Link></div>
-  ;
+  return (
+    <div className="w-full bg-gray-100">
+      <Link to="/dashboard">Dashboard</Link>
+    </div>
+  );
 };
 Recordings.defaultProps = {};
 
