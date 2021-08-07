7ef5f0b6ba98c20accb576986bedbbab71137290
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-08-07 12:36:34 +0530

    change some styling of sidebar and add Groups tab in it

--- src/components/SideBar/Sidebar.tsx
+++ src/components/SideBar/Sidebar.tsx
@@ -3,11 +3,15 @@ import SidebarTab from "./SidebarTab";
 import { AiOutlineHome } from "react-icons/ai";
 import { logout } from "../../api";
 import { BiLogOut, BiVideoRecording } from "react-icons/bi";
+import { HiUserGroup } from "react-icons/hi";
 import { useHistory } from "react-router-dom";
 import { Transition } from "@headlessui/react";
 import { useAppSelector } from "../../store";
 import { headerActions } from "../../actions/header.actions";
-import { sidebarOpenSelector } from "../../selectors/ui.selectors";
+import {
+  headerTitleSelector,
+  sidebarOpenSelector,
+} from "../../selectors/ui.selectors";
 interface Props {}
 const Sidebar: React.FC<Props> = (props) => {
   const isSidebarOpen = useAppSelector((state) => sidebarOpenSelector(state));
@@ -21,6 +25,11 @@ const Sidebar: React.FC<Props> = (props) => {
     history.push("/dashboard");
     headerActions.uiHeaderTitleChangeAction("Dashboard");
   };
+  const goToGroups = () => {
+    headerActions.uiHeaderTitleChangeAction("Groups");
+  };
+  const headerTitle = useAppSelector((state) => headerTitleSelector(state));
+
   return (
     <div>
       <Transition
@@ -34,16 +43,31 @@ const Sidebar: React.FC<Props> = (props) => {
         leaveTo="-translate-x-full "
       >
         <div
-          className=" w-60 z-30  bg-gray-200 flex sticky top-28  flex-col p-3 "
+          className=" w-52 z-30  bg-gray-200 flex sticky top-28  flex-col p-3 "
           style={{ height: "calc(100vh - 112px)" }}
         >
-          <SidebarTab Icon={AiOutlineHome} handleClick={goToDashboard}>
+          <SidebarTab
+            Icon={AiOutlineHome}
+            handleClick={goToDashboard}
+            className={headerTitle === "Dashboard" ? "bg-white" : ""}
+          >
             Dashboard
           </SidebarTab>
-          <SidebarTab Icon={BiVideoRecording} handleClick={goToRecordings}>
+          <SidebarTab
+            Icon={BiVideoRecording}
+            handleClick={goToRecordings}
+            className={headerTitle === "Recordings" ? "bg-white" : ""}
+          >
             Recordings
           </SidebarTab>
-          <SidebarTab Icon={BiLogOut} handleClick={logout}>
+          <SidebarTab
+            Icon={HiUserGroup}
+            handleClick={goToGroups}
+            className={headerTitle === "Groups" ? "bg-white" : ""}
+          >
+            Groups
+          </SidebarTab>
+          <SidebarTab Icon={BiLogOut} handleClick={logout} className={""}>
             Logout
           </SidebarTab>
         </div>
--- src/components/SideBar/SidebarTab.tsx
+++ src/components/SideBar/SidebarTab.tsx
@@ -3,15 +3,24 @@ import { IconType } from "react-icons";
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   Icon?: IconType;
   handleClick?: () => void;
+  className: string;
 }
-const SidebarTab: React.FC<Props> = ({ Icon, handleClick, children }) => {
+const SidebarTab: React.FC<Props> = ({
+  Icon,
+  handleClick,
+  children,
+  className,
+}) => {
   return (
     <button
-      className="flex px-4 py-3 my-1 border border-gray-500 items-center hover:bg-gray-400 rounded-lg"
+      className={
+        "flex px-4 py-3 my-1 items-center text-gray-600 hover:bg-gray-300 hover:text-black rounded-lg  duration-500 ease-in-out transition-colors " +
+        className
+      }
       onClick={handleClick}
     >
-      {Icon && <Icon className="h-6 w-6"></Icon>}
-      <h3 className="text-md font-semibold pl-4">{children}</h3>
+      {Icon && <Icon className="h-6 w-6 "></Icon>}
+      <h3 className="text-md font-semibold pl-4 ">{children}</h3>
     </button>
   );
 };
